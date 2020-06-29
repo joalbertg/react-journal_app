f095b4736facf68b1162afb43928c184db4ab788
Joalbert <joalbertgonzalez@gmail.com>
2020-06-29 18:10:01 -0500

    first commit 🚀

---
+++ src/JournalApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/styles.scss';
+
+const JournalApp = () => {
+  return(
+    <>
+      <h1>Journal App</h1>
+      <hr />
+    </>
+  );
+}
+
+export default JournalApp;
+
