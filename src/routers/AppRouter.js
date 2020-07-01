4e267e711ebd73b0655354f0acf4a932144008be
Joalbert <joalbertgonzalez@gmail.com>
2020-06-30 22:48:27 -0500

    [ADD]🚀 Remember user with observable added

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,8 +9,33 @@ import {
 
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+import { firebase } from '../firebase/firebase-config';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // firebase.auth().onAuthStateChanged()
+    // this is an observable, will keep the user's data
+    // when reloading the page
+    firebase.auth().onAuthStateChanged(user => {
+      if(user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      }
+      setChecking(false);
+    });
+  }, [dispatch, setChecking, setIsLoggedIn]);
+
+  if(checking) {
+    return(
+      <h2>Checking...</h2>
+    );
+  }
+
   return(
     <Router>
       <div>
