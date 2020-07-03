5ff237c22d2c42e2afc075c0c5d16d8c92bbdd09
Joalbert <joalbertgonzalez@gmail.com>
2020-07-02 23:55:22 -0500

    Updates

---
+++ precache-manifest.95e28c7b7f62cca03834afd4fc672950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95fbc1055021b65566b12ad76112209",
+    "url": "/react-journal_app/index.html"
+  },
+  {
+    "revision": "4e3f6c4b9f5535f4382a",
+    "url": "/react-journal_app/static/css/main.4abcfe25.chunk.css"
+  },
+  {
+    "revision": "d6c6b5fc82a3e328873f",
+    "url": "/react-journal_app/static/js/2.fe38b1f3.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/react-journal_app/static/js/2.fe38b1f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3f6c4b9f5535f4382a",
+    "url": "/react-journal_app/static/js/main.82b1cfc7.chunk.js"
+  },
+  {
+    "revision": "aee5ee5c75eb1ed9d9ea",
+    "url": "/react-journal_app/static/js/runtime-main.52be2eff.js"
+  }
+]);
